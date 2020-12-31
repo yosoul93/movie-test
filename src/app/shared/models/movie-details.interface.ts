373b3be21a15adaa64c97e0e723c6724c50c138e
kelvin <p_ohagwaekene.kelvin@diva.co.jp>
2020-12-31 17:07:11 +0700

    created models for http responses

---
+++ src/app/shared/models/movie-details.interface.ts
@@ -0,0 +1,27 @@
+export interface MovieDetails {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: object;
+  budget: number;
+  genres: [{id: number, name: string}];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: [{id: number, name: string, logo_path: string, }];
+  production_countries: [{id: number, name: string, logo_path: string, }];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: [{name: string}];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+}
