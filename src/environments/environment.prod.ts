f73e5e5be0550a1b04fabaee0f8e2ea6549245e1
kelvin <p_ohagwaekene.kelvin@diva.co.jp>
2021-01-01 00:13:01 +0700

    added url to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3/',
+  apiKey: 'db33a6cabc8d0c62560eb674b9844a5f',
+  imgUrl: 'https://image.tmdb.org/t/p/w500/'
 };
