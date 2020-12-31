25243f1993cc55a3ceff6dd7666ace12bf243f52
kelvin <p_ohagwaekene.kelvin@diva.co.jp>
2020-12-31 17:08:11 +0700

    added url, apikey and imgUrl to the environemnt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,11 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3/',
+  apiKey: 'db33a6cabc8d0c62560eb674b9844a5f',
+  imgUrl: 'https://image.tmdb.org/t/p/w500/'
+
 };
 
 /*
